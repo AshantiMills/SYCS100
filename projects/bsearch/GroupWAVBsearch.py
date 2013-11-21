ee904fc45a198e17bc532dd85e30039d74c21d07
AshantiMills <ashanti.mills@bison.howard.edu>
2013-11-20 21:23:49 -0500

    Ashanti's BSearch Code
    I added my bsearch.py code, from lines 9-31.

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -4,3 +4,28 @@
 #Eboni
 #Ashanti
 #Tyler
+
+
+list = [1,2,64,78,79,80,100,121,144,387,4000]
+x = 387
+def bsearch(list,x):
+	if list != None:
+		hi = len(list)
+		lo = 0
+		n = (hi+lo)/2
+		a = 0
+		while list[n] != x and a < len(list):
+			if list[n] < x:
+				lo = n
+			elif list[n] > x:
+				hi = n
+			n = (hi+lo)/2
+			a += 1
+		
+		if list[n] == x:
+			print n
+		else:
+			print -1
+	else:
+		print -1	
+bsearch(list,x)
