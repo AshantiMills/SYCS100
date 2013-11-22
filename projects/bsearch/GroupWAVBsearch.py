ebef1dbc3993142ecf8b3a5751a17ae6233c96bb
AshantiMills <ashanti.mills@bison.howard.edu>
2013-11-22 02:15:41 -0500

    Ashanti's BSearch Code
    I set up the function so that it only works for lists that aren't null; otherwise it skips to the end at prints -1. To stop it from going on forever, should it get close to the answer but not quite, I only allowed my while loop to perform as many times as there are iterations in the list.

--- projects/bsearch/GroupWAVBsearch.py
+++ projects/bsearch/GroupWAVBsearch.py
@@ -4,3 +4,25 @@
 #Eboni
 #Ashanti
 #Tyler
+
+def bsearch(list,x):
+	if list != None:
+		hi = len(list)
+		lo = 0
+		n = (hi+lo)/2
+		a = 0
+		while list[n] != x and a < len(list):
+			if list[n] < x:
+				lo = n
+			elif list[n] > x:
+				hi = n
+			n = (hi+lo)/2
+			a += 1
+		
+		if list[n] == x:
+			print n
+		else:
+			print -1
+	else:
+		print -1	
+bsearch(list,x)
